58681f7b4e8dd3fbe79bd20302e67dfce30cee93
pelumzeny <pelumzeny10@gmail.com>
2019-02-28 04:00:30 +0100

    completed the application

--- FingerGuess/ViewController.swift
+++ FingerGuess/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtNumber: UITextField!
+    @IBOutlet weak var display_text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func makeGuess(_ sender: Any) {
+        let randomInt = Int.random(in: 0..<6)
+        //let cc = arc4random_uniform(9)
+        let numberGuess = txtNumber.text
+        
+        if Int(numberGuess!)==randomInt {
+            display_text.text = "You're right!"
+        }else{
+            display_text.text = "Wrong! It was a \(randomInt)"
+        }
+    }
+    
 }
 
